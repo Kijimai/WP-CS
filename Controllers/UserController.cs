6d36c8f5d35ad15fdfd15a21db3feb17a0e861d4
John Deen Bucog <JDBucog@yahoo.com>
2022-06-20 18:27:03 -0700

    Login and Register, basic Dashboard redirect page

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -41,10 +41,52 @@ public class UserController : Controller
     PasswordHasher<User> Hasher = new PasswordHasher<User>();
     newUser.Password = Hasher.HashPassword(newUser, newUser.Password);
 
-    
-
     _context.Users.Add(newUser);
     _context.SaveChanges();
+
+    HttpContext.Session.SetInt32("UserId", newUser.UserId);
+
     return RedirectToAction("Dashboard");
+  }
+
+  [HttpPost("/login")]
+  public IActionResult Login(LoginUser loginUser)
+  {
+    if (ModelState.IsValid == false)
+    {
+      ViewBag.AuthorizationError = "Your email or password is incorrect!";
+      return LoginAndRegister();
+    }
+
+    User? foundUser = _context.Users.FirstOrDefault(u => u.Email == loginUser.LoginEmail);
+
+    if (foundUser == null)
+    {
+      ModelState.AddModelError("Email", "and Password do not match.");
+      return LoginAndRegister();
+    }
+
+    PasswordHasher<LoginUser> Hasher = new PasswordHasher<LoginUser>();
+    PasswordVerificationResult comparePassword = Hasher.VerifyHashedPassword(loginUser, foundUser.Password, loginUser.LoginPassword);
+
+    if (comparePassword == 0)
+    {
+      ModelState.AddModelError("Email", "and Password don't match!");
+      return LoginAndRegister();
+    }
+
+    HttpContext.Session.SetInt32("UserId", foundUser.UserId);
+    return RedirectToAction("Dashboard");
+  }
+
+  [HttpGet("dashboard")]
+  public IActionResult Dashboard()
+  {
+    int? loginUserID = HttpContext.Session.GetInt32("UserId");
+    if (loginUserID == null)
+    {
+      return LoginAndRegister();
+    }
+    return View("Dashboard");
   }
 }
